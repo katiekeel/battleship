d53b84087d663ad56c855b049786d672cc504b5a
Katie Shermer <katieshermer@gmail.com>
2017-05-22 14:51:06 -0600

    Partial three-unit validation

--- lib/board.rb
+++ lib/board.rb
@@ -6,7 +6,7 @@ class Board
 
   attr_reader :equals_nums_rows, :a_row, :b_row, :c_row, :d_row
 
-  attr_accessor :computer_game_board, :player_game_board
+  attr_accessor :game_board, :display_board
 
   def initialize
 
@@ -25,20 +25,8 @@ class Board
     @d_row = {:D => "D", :D1 => " ", :D2 => " ", :D3 => " ", :D4 => " "}
   end
 
-  def computer_game_board
-    computer_game_board = [
-      @equals_nums_rows[:top_equals_row],
-      @equals_nums_rows[:nums_row],
-      @a_row,
-      @b_row,
-      @c_row,
-      @d_row,
-      @equals_nums_rows[:bottom_equals_row]
-    ]
-  end
-
-  def computer_display_board
-    computer_display_board = [
+  def display_board
+    display_board = [
       @equals_nums_rows[:top_equals_row],
       @equals_nums_rows[:nums_row],
       @a_row.values.join(""),
@@ -49,56 +37,63 @@ class Board
     ]
   end
 
-  def player_game_board
-    player_game_board = [
-      @a_row,
-      @b_row,
-      @c_row,
-      @d_row
-    ]
+  def game_board
+    game_board = [@a_row, @b_row, @c_row, @d_row]
   end
 
-  def player_display_board
-    player_display_board = [
-      @equals_nums_rows[:top_equals_row],
-      @equals_nums_rows[:nums_row],
-      @a_row.values.join(""),
-      @b_row.values.join(""),
-      @c_row.values.join(""),
-      @d_row.values.join(""),
-      @equals_nums_rows[:bottom_equals_row]
-    ]
-  end
 
-  def computer_ship_placement
-    coords = get_random_keys.map do |item|
-      item.sample
+    def two_unit_player_ship_placement(player)
+      coords = player.ship_placement
+      if two_unit_valid?(coords)
+        coords.each do |coord|
+          game_board.each do |item|
+            item[coord] = "X" if item.key?(coord) && item[coord] != "X"
+          end
+        end
+      else
+        puts "Invalid coords, try again"
+        two_unit_player_ship_placement(player)
+      end
+      puts display_board
     end
-    coords = coords.sample(2)
-    # binding.pry
-    until valid?(coords)
-      coords = []
-    get_random_keys.each do |item|
-      coords << item.sample unless coords.length == 2
-    end
+
+  def two_unit_computer_ship_placement
+    coords = get_random_keys.sample(2)
+    until two_unit_valid?(coords)
+      coords = get_random_keys.sample(2)
     end
     coords.each do |coord|
-      computer_game_board.each do |item|
-        next if item.is_a? String
+      game_board.each do |item|
         if item.key?(coord)
           item[coord] = "X"
         end
       end
     end
-    puts computer_display_board
-    computer_display_board
+    puts display_board
+    display_board
   end
 
-  def valid?(coords)
+
+    def get_random_keys
+      random_keys = []
+      game_board.each do |thing|
+        next if thing.is_a? String
+        random_keys << thing.keys
+      end
+      random_keys.map! do |item|
+        item.drop_while do |thing|
+          thing == :A || thing == :B || thing == :C || thing == :D
+        end
+      end
+      random_keys.map! do |item|
+        item.sample
+      end
+      random_keys
+    end
+
+  def two_unit_valid?(coords)
     first_coord = coords[0]
     second_coord = coords[1]
-    # check coord string vs index 0 & 1 of key
-    # binding.pry
     if first_coord.nil? || second_coord.nil?
       false
     elsif first_coord[0] == second_coord[0]
@@ -120,35 +115,46 @@ class Board
     end
   end
 
-  def get_random_keys
-    random_keys = []
-    computer_game_board.each do |thing|
-      next if thing.is_a? String
-      random_keys << thing.keys
-    end
-    random_keys.map! do |item|
-      item.drop_while do |thing|
-        thing == :A || thing == :B || thing == :C || thing == :D
+  def three_unit_valid?(coords)
+    first_coord = coords[0]
+    second_coord = coords[1]
+    if first_coord.nil? || second_coord.nil?
+      false
+    elsif first_coord[0] == second_coord[0]
+      if second_coord[1].to_i == first_coord[1].to_i - 2 || first_coord[1].to_i + 2
+        true unless
+    elsif second_coord[0].ord == first_coord[0].ord + 1
+      if second_coord[1] == first_coord[1].to_i - 1
+       true
+      else
+        false
       end
-    end
-    random_keys
-  end
-
-  def two_unit_player_ship_placement(player)
-    coords = player.ship_placement
-    binding.pry
-    if valid?(coords)
-      coords.each do |coord|
-        player_game_board.each do |item|
-          item[coord] = "X" if item.key?(coord) && item[coord] != "X"
-        end
+    elsif second_coord[0].ord == first_coord[0].ord - 1
+      if second_coord[1] == first_coord[1]
+        true
+      else
+        false
       end
     else
-      puts "Invalid coords, try again"
-      two_unit_player_ship_placement(player)
+      false
     end
-    puts player_display_board
+
   end
 
+  def three_unit_computer_ship_placement
+    coords = get_random_keys.sample(2)
+    until three_unit_valid?(coords)
+      coords = get_random_keys.sample(2)
+    end
+    coords.each do |coord|
+      game_board.each do |item|
+        if item.key?(coord)
+          item[coord] = "X"
+        end
+      end
+    end
+    puts display_board
+    display_board
+  end
 
 end
--- test/temp_test.rb
+++ test/temp_test.rb
@@ -4,5 +4,5 @@ require './lib/board.rb'
 computer_board = Board.new
 player_board = Board.new
 player = Player.new
-# board.computer_ship_placement
+computer_board.two_unit_computer_ship_placement
 player_board.two_unit_player_ship_placement(player)
